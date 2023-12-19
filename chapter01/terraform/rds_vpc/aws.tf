df36af8a46f662fae1f74da40909a11038298e93
Rhys Campbell <rhyscampbell@bluewin.ch>
2023-12-19 20:46:37 +0100

    Terraform format

--- chapter01/terraform/rds_vpc/aws.tf
+++ chapter01/terraform/rds_vpc/aws.tf
@@ -1,13 +1,13 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~>5.31.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.31.0"
     }
+  }
 }
 
 provider "aws" {
-    region  = "eu-central-1"
-    profile = "aws_terraform"
+  region  = "eu-central-1"
+  profile = "aws_terraform"
 }
