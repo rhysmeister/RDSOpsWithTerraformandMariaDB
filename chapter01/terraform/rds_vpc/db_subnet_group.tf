dcd2e9c5c8a28116c58d472c84bcfff01f92f092
Rhys Campbell <rhyscampbell@bluewin.ch>
2023-12-20 20:41:22 +0100

    Terraform format

--- chapter01/terraform/rds_vpc/db_subnet_group.tf
+++ chapter01/terraform/rds_vpc/db_subnet_group.tf
@@ -1,8 +1,8 @@
 resource "aws_db_subnet_group" "rds_subnet_group" {
-    name = "rds-db-subnet-group"
-    subnet_ids = [
-        aws_subnet.private_subnet1.id,
-        aws_subnet.private_subnet2.id,
-        aws_subnet.private_subnet3.id
-    ]
+  name = "rds-db-subnet-group"
+  subnet_ids = [
+    aws_subnet.private_subnet1.id,
+    aws_subnet.private_subnet2.id,
+    aws_subnet.private_subnet3.id
+  ]
 }
